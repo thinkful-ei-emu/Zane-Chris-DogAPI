6348632aa111dd0ca131ec01b8a530b1dd3e5747
Zane Kuehn <zanekuehn@yahoo.com>
2019-06-11 13:29:06 -0500

    Finished excersize 1

--- index.js
+++ index.js
@@ -6,31 +6,41 @@ function handleSubmitClick() {
   $('#js-num-form').submit(event => {
     event.preventDefault();
     console.log('`handleSubmitClick` ran');
-    const dogNumb = $('js-num-input').val();
+    const dogNumb = $('.js-num-input').val();
+    console.log(dogNumb);
     getDogImages(dogNumb);
+    
+    
+    
   });
 }
 
 //get the images
 function getDogImages(dogNumb) {
+  //console.log(number);
   console.log('`getDogImages` ran');
-  let i = 1;
-  let fetchNow = fetch('https://dog.ceo/api/breeds/image/random')
-  if(i<=dogNumb) {
-    fetchNow
-      .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
-      .catch(error => alert('Something went wrong. Try again later.'));
-    i++;
-  }
+  console.log();
+  let fetchNow = fetch(`https://dog.ceo/api/breeds/image/random/${dogNumb}`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+  
+}
     
+function pullData(JsonData){
+  JsonData.forEach(dog=>{
+
+  });
+
 }
 
 //display the dog Amounts
 function displayResults(responseJson) {
   console.log('`displayResults` ran');
   console.log(responseJson);
+
+ 
   //display the results section
   $('.results').removeClass('hidden');
 }
@@ -38,4 +48,6 @@ function displayResults(responseJson) {
 //call the final function
 $(function() {
   handleSubmitClick();
+  
+ 
 });
